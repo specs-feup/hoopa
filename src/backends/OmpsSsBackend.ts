b10192ba124911319f07cd2916a121d0a0204084
tiagolascasas <tiagolascasas@outlook.com>
2025-03-13 23:40:12 +0000

    Skeleton of OmpSs backend

---
+++ src/backends/OmpsSsBackend.ts
@@ -0,0 +1,16 @@
+import { FunctionJp, Scope } from "@specs-feup/clava/api/Joinpoints.js";
+import { ABackend } from "./ABackend.js";
+import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js";
+import { ClusterInOut } from "@specs-feup/extended-task-graph/Cluster";
+
+export class OmpSsBackend extends ABackend {
+    constructor(topFunctionName: string, outputDir: string, appName: string) {
+        super(topFunctionName, outputDir, appName, "XRT");
+    }
+
+    protected buildBody(wrapperFun: FunctionJp, entrypoint: string, inOuts: Map<string, ClusterInOut>, debug: boolean): Scope {
+        this.logWarning("OmpSs backend not implemented yet, outputting an empty wrapper function");
+        const body = ClavaJoinPoints.scope();
+        return body;
+    }
+}
