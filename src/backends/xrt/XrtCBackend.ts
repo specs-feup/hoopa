e90dbc6d4b4eee29bc44cf75d92f110e8f8eed03
tiagolascasas <tiagolascasas@outlook.com>
2026-01-24 05:06:40 +0000

    Refactors transformations in XRT backend

--- src/backends/xrt/XrtCBackend.ts
+++ src/backends/xrt/XrtCBackend.ts
@@ -1,13 +1,12 @@
 import { FunctionJp, Scope } from "@specs-feup/clava/api/Joinpoints.js";
 import { ABackend } from "../ABackend.js";
-import Query from "@specs-feup/lara/api/weaver/Query.js";
 import { CallTreeInliner } from "@specs-feup/clava-code-transforms/CallTreeInliner";
 import { SourceCodeOutput } from "@specs-feup/extended-task-graph/OutputDirectories";
 import Clava from "@specs-feup/clava/api/clava/Clava.js";
 import { MallocHoister } from "@specs-feup/clava-code-transforms/MallocHoister";
 import { StructFlattener } from "@specs-feup/clava-code-transforms/StructFlattener";
 import { LightStructFlattener } from "@specs-feup/clava-code-transforms/LightStructFlattener";
-import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js";
+import { HlsDeadCodeEliminator } from "./HlsDeadCodeEliminator.js";
 
 export class XrtCBackend extends ABackend {
     constructor(topFunctionName: string, outputDir: string, appName: string) {
@@ -15,80 +14,68 @@ export class XrtCBackend extends ABackend {
     }
 
     protected applyTransforms(clusterFun: FunctionJp, bridgeFun: FunctionJp, folderName: string): [FunctionJp, FunctionJp] {
-        // let fun = this.regenClusterFunction(clusterFun.name);
-        // const inlined = this.applyInlining(fun, folderName);
-        // if (!inlined) {
-        //     this.log("Skipping remaining transforms due to inlining failure");
-        //     return this.regenClusterFunction(clusterFun.name);
-        // }
+        const getClusterFun = () => this.regenFunction(clusterFun.name);
+        const getBridgeFun = () => this.regenFunction(bridgeFun.name);
 
-        // fun = this.regenClusterFunction(clusterFun.name);
-        // const hoisted = this.applyMallocHoisting(fun, folderName);
-        // if (!hoisted) {
-        //     this.log("Skipping remaining transforms due to malloc hoisting failure");
-        //     return this.regenClusterFunction(clusterFun.name);
-        // }
+        const steps = [
+            { name: "DCE", apply: () => this.applyDeadCodeElimination(getClusterFun(), folderName) },
+            { name: "inlining", apply: () => this.applyInlining(getClusterFun(), folderName) },
+            { name: "struct flattening", apply: () => this.applyStructFlattening(getClusterFun(), folderName) },
+            { name: "malloc hoisting", apply: () => this.applyMallocHoisting(getClusterFun(), folderName) },
+        ];
 
-        // fun = this.regenClusterFunction(clusterFun.name);
-        // const flattened = this.applyStructFlattening(fun, folderName);
-        // if (!flattened) {
-        //     this.log("Skipping remaining transforms due to struct flattening failure");
-        //     return this.regenClusterFunction(clusterFun.name);
-        // }
+        for (const step of steps) {
+            if (!step.apply()) {
+                this.log(`Skipping remaining transforms due to ${step.name} failure`);
+                return [getClusterFun(), getBridgeFun()];
+            }
+        }
+        return [getClusterFun(), getBridgeFun()];
+    }
 
-        return [this.regenFunction(clusterFun.name), this.regenFunction(bridgeFun.name)];
+    private applyPass(name: string, transform: (f: FunctionJp) => void, fun: FunctionJp, folder: string, step: string): boolean {
+        this.log(`Applying ${name}`);
+        try {
+            transform(fun);
+            this.generateCode(`${SourceCodeOutput.SRC_PARENT}/${folder}/${step}`);
+            this.log(`Code generated at ${SourceCodeOutput.SRC_PARENT}/${folder}/${step}`);
+            Clava.rebuild();
+            return true;
+        }
+        catch (e) {
+            this.logError(`Error during ${name}: ${e}`);
+            return false;
+        }
+    }
+
+    private applyDeadCodeElimination(clusterFun: FunctionJp, folderName: string): boolean {
+        return this.applyPass("dead code elimination", (fun) => {
+            new HlsDeadCodeEliminator().removeAll(fun);
+        }, clusterFun, folderName, "t1-dead-code-elimination");
     }
 
     private applyInlining(clusterFun: FunctionJp, folderName: string): boolean {
-        this.log("Applying call tree inlining");
-        try {
+        return this.applyPass("call tree inlining", (fun) => {
             const inliner = new CallTreeInliner();
-            inliner.inlineCallTree(clusterFun, true);
+            inliner.inlineCallTree(fun, true);
             Clava.rebuild();
 
-            clusterFun = this.regenFunction(clusterFun.name);
-            inliner.revertGlobalsToParams(clusterFun);
-
-            this.generateCode(`${SourceCodeOutput.SRC_PARENT}/${folderName}/t1-inline`);
-            this.log(`Code generated at ${SourceCodeOutput.SRC_PARENT}/${folderName}/t1-inline`);
-            Clava.rebuild();
-            return true;
-        }
-        catch (e) {
-            this.logError(`Error during call tree inlining: ${e}`);
-            return false;
-        }
-    }
-
-    private applyMallocHoisting(clusterFun: FunctionJp, folderName: string): boolean {
-        this.log("Applying malloc hoisting");
-        try {
-            const hoister = new MallocHoister();
-            hoister.hoistAllMallocs(clusterFun);
-            this.generateCode(`${SourceCodeOutput.SRC_PARENT}/${folderName}/t2-hoisting`);
-            this.log(`Code generated at ${SourceCodeOutput.SRC_PARENT}/${folderName}/t2-hoisting`);
-            Clava.rebuild();
-            return true;
-        }
-        catch (e) {
-            this.logError(`Error during malloc hoisting: ${e}`);
-            return false;
-        }
+            const newFun = this.regenFunction(fun.name);
+            inliner.revertGlobalsToParams(newFun);
+        }, clusterFun, folderName, "t2-inline");
     }
 
     private applyStructFlattening(clusterFun: FunctionJp, folderName: string): boolean {
-        this.log("Applying struct flattening");
-        try {
-            const flat = new StructFlattener(new LightStructFlattener());
-            flat.flattenAll(clusterFun);
-            this.generateCode(`${SourceCodeOutput.SRC_PARENT}/${folderName}/t3-flattening`);
-            this.log(`Code generated at ${SourceCodeOutput.SRC_PARENT}/${folderName}/t3-flattening`);
-            Clava.rebuild();
-            return true;
-        }
-        catch (e) {
-            this.logError(`Error during struct flattening: ${e}`);
-            return false;
-        }
+        return this.applyPass("struct flattening", (fun) => {
+            new StructFlattener(new LightStructFlattener()).flattenAll(fun);
+        }, clusterFun, folderName, "t3-flattening");
     }
+
+    private applyMallocHoisting(clusterFun: FunctionJp, folderName: string): boolean {
+        return this.applyPass("malloc hoisting", (fun) => {
+            new MallocHoister().hoistAllMallocs(fun);
+        }, clusterFun, folderName, "t4-hoisting");
+    }
+
+
 }
